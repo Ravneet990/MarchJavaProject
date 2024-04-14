fb2711f71a0ee34b7228675af816d4be633372be
ravneetkaur990 <rav95k@gmail.com>
2024-04-13 21:36:44 -0400

    Changes made to username password assignment

--- src/usernamePasswordAssignment/usernamePassword.java
+++ src/usernamePasswordAssignment/usernamePassword.java
@@ -19,53 +19,44 @@ public class usernamePassword {
 	Scanner sc1 = new Scanner(System.in);
 
 	public void compareUsernamePassword() {
-		if (maxAttempts != 0) {
 
-			for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 
 //		while (true) {
 
-				System.out.println("Enter username");
+			System.out.println("Enter username");
 
-				inputusername = sc.next();
+			inputusername = sc.next();
 
-				System.out.println("Enter password");
+			System.out.println("Enter password");
 
-				inputpassword = sc1.next();
+			inputpassword = sc1.next();
 
-				if (inputusername.equals(null) && (inputpassword.equals(null)))
+			if ((inputusername.equals(userID)) && (inputpassword.equals(password)))
 
-				{
+			{
 
-					System.out.println("Enter username and password");
+				System.out.println("You are logged in to the application.");
+				break;
 
 //				break;
 
-				} else if ((inputusername.equals(userID)) && (inputpassword.equals(password)))
-
-				{
-
-					System.out.println("You are logged in to the application.");
-
-					break;
-
-				}
-
-				else {
-
-					System.out.println("Incorrect UserID or Password. Please try again.");
-
-					maxAttempts--;
-
-				}
 			}
+
+			else if ((!inputusername.equals(userID)) && (!inputpassword.equals(password)))
+
+				System.out.println("Incorrect UserID or Password. Please try again.");
+
+			maxAttempts--;
+
 		}
 
-		else if (maxAttempts == 0) {
+		if (maxAttempts == 0) {
 
-			System.out.println("Max attempts reached;");
+			System.out.println("Max attempts reached. Your account is blocked");
 //				break;
 		}
 
 	}
+
 }
