87abb4dc15af12569b618ff0da7668d398c210c0
ravneetkaur990 <rav95k@gmail.com>
2024-04-14 00:38:52 -0400

    Water Tank assignment added

---
+++ src/waterTankAssignment/WaterTank.java
@@ -0,0 +1,63 @@
+package waterTankAssignment;
+
+import java.util.Scanner;
+
+public class WaterTank {
+
+	int waterTankCapacity = 0;
+
+	int bucket = 10;
+
+	int counter = 10;
+
+	Scanner sc = new Scanner(System.in);
+
+	public void fillWaterTank() {
+
+		System.out.println("The capacity of water tank in litres right now is :  " + waterTankCapacity);
+
+		if (counter != 0) {
+
+			for (int i = 10; i <= 100; i++) {
+
+				System.out.println("Do you want to add more water into the tank with bucket?");
+
+				String addWater = sc.next();
+
+				if (addWater.equalsIgnoreCase("yes")) {
+
+					waterTankCapacity = waterTankCapacity + 10;
+
+					System.out.println("The capacity of Water right now is : " + waterTankCapacity);
+
+					counter--;
+
+					if (counter == 0) {
+
+						System.out.println("You have reached the maximum capacity of Water Tank");
+
+						break;
+					}
+
+				}
+
+				else if (addWater.equalsIgnoreCase("no")) {
+
+					System.out.println("The Final Capacity of Water in litres is :" + waterTankCapacity);
+
+					break;
+
+				}
+
+//			else {
+//				
+//				System.out.println("The Final Capacity of Water is :" + waterTankCapacity);
+//			}
+
+			}
+
+		}
+
+	}
+
+}
---
+++ src/waterTankAssignment/WaterTankMain.java
@@ -0,0 +1,13 @@
+package waterTankAssignment;
+
+public class WaterTankMain {
+
+	public static void main(String[] args) {
+
+		WaterTank wt = new WaterTank();
+
+		wt.fillWaterTank();
+
+	}
+
+}
